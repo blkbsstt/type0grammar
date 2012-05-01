bb925e2c519af189990d1b7f797fe7da80c1a3c0
Blake Bassett <blkbsstt@gmail.com>
2012-05-01 12:04:19 -0500

    Added the ability to use a custom separator.

--- src/main/scala/thue.scala
+++ src/main/scala/thue.scala
@@ -1,39 +1,43 @@
 object Thue {
 	import util.Random
 
-	val sep = " -> "
+	val defaultSep = " -> "
 
-	case class Rule(lhs: String, rhs: String) {
+	case class Rule(lhs: String, rhs: String, sep: String) {
 		override def toString = lhs + sep + rhs
 	}
 
 	def main(args: Array[String]): Unit = {
 		var opts = options(args.toList)
+		opts = opts.updated("sep", opts.get("sep").getOrElse(defaultSep))
 
 		if (!(opts contains "file")) return println(usage)
 
-		val (state, rules) = parse(opts("file"))
+		val (state, rules) = parse(opts("file"), opts("sep"))
 
 		if (opts contains "debug") println(rules.mkString("\n"))
 
 		if (!((opts contains "batch") || (opts contains "times")))
 			opts += ("batch" -> "1")
 
-		println(batch(state, rules, opts).toList.sorted.mkString("\n"))
+		val strings = batch(state, rules, opts)
+		if (!(opts contains "unsorted")) println(strings.toList.sorted.mkString("\n"))
 	}
 
-	val usage = """thue [-d] [-[Bb] num] [-t num] <program_file>"""
+	val usage = """scala thue.scala [-b num] [-t num] [-d] [-u] <program_file>"""
 
 	def options(args: List[String], opts: Map[String, String] = Map.empty): Map[String, String] =
 		args match {
 			case "-b" :: i :: rest ⇒ options(rest, opts + ("batch" -> i))
 			case "-t" :: i :: rest ⇒ options(rest, opts + ("times" -> i))
+			case "-s" :: sep :: rest => options(rest, opts + ("sep" -> sep))
 			case "-d" :: rest ⇒ options(rest, opts + ("debug" -> "true"))
+			case "-u" :: rest ⇒ options(rest, opts + ("unsorted" -> "true"))
 			case file :: Nil ⇒ opts + ("file" -> file)
 			case _ ⇒ opts
 		}
 
-	def parse(filename: String) = {
+	def parse(filename: String, sep: String) = {
 		val input = io.Source.fromFile(filename).getLines
 		val state = input.next
 
@@ -43,7 +47,7 @@ object Thue {
 		val Whitespace = """\s+""".r
 
 		val rules = input.flatMap {
-			case InputRegEx(lhs, rhs) ⇒ Some(Rule(lhs, rhs))
+			case InputRegEx(lhs, rhs) ⇒ Some(Rule(lhs, rhs, sep))
 			case Whitespace ⇒ None
 			case line ⇒ println("Malformed production: " + line); None
 		} toList
@@ -66,7 +70,11 @@ object Thue {
 		def loop(strings: Set[String], t: Int): Set[String] =
 			if (((opts contains "batch") && (strings.size >= opts("batch").toInt))
 				|| ((opts contains "times") && (t >= opts("times").toInt))) strings
-			else loop(strings + execute(state, rules, opts), t + 1)
+			else {
+				val s = execute(state, rules, opts)
+				if ((opts contains "unsorted") && !strings(s)) println(s)
+				loop(strings + s, t + 1)
+			}
 		loop(Set.empty[String], 0)
 	}
 }
