42ab8175c4003540d1bb4180b7cf505b5a2c1d32
Blake Bassett <blkbsstt@gmail.com>
2012-05-01 10:55:53 -0500

    Improved some formatting.

--- src/main/scala/thue.scala
+++ src/main/scala/thue.scala
@@ -4,33 +4,33 @@ object Thue {
 	val sep = " -> "
 
 	case class Rule(lhs: String, rhs: String) {
-		override def toString = lhs + sep + rhs 
+		override def toString = lhs + sep + rhs
 	}
 
-	def main(args: Array[String]) : Unit = {
+	def main(args: Array[String]): Unit = {
 		var opts = options(args.toList)
-		
+
 		if (!(opts contains "file")) return println(usage)
-		
+
 		val (state, rules) = parse(opts("file"))
-		
+
 		if (opts contains "debug") println(rules.mkString("\n"))
-		
+
 		if (!((opts contains "batch") || (opts contains "times")))
 			opts += ("batch" -> "1")
-			
+
 		println(batch(state, rules, opts).toList.sorted.mkString("\n"))
 	}
 
 	val usage = """thue [-d] [-[Bb] num] [-t num] <program_file>"""
 
-	def options(args: List[String], opts: Map[String, String] = Map.empty) : Map[String, String] = 
+	def options(args: List[String], opts: Map[String, String] = Map.empty): Map[String, String] =
 		args match {
-			case "-b" :: i :: rest => options(rest, opts + ("batch" -> i))
-			case "-t" :: i :: rest => options(rest, opts + ("times" -> i))
-			case "-d" :: rest => options(rest, opts + ("debug" -> "true"))
-			case file :: Nil => opts + ("file" -> file)
-			case _ => opts
+			case "-b" :: i :: rest ⇒ options(rest, opts + ("batch" -> i))
+			case "-t" :: i :: rest ⇒ options(rest, opts + ("times" -> i))
+			case "-d" :: rest ⇒ options(rest, opts + ("debug" -> "true"))
+			case file :: Nil ⇒ opts + ("file" -> file)
+			case _ ⇒ opts
 		}
 
 	def parse(filename: String) = {
@@ -43,17 +43,17 @@ object Thue {
 		val Whitespace = """\s+""".r
 
 		val rules = input.flatMap {
-			case InputRegEx(lhs, rhs) => Some(Rule(lhs, rhs))
-			case Whitespace => None
-			case line => println("Malformed production: " + line); None
+			case InputRegEx(lhs, rhs) ⇒ Some(Rule(lhs, rhs))
+			case Whitespace ⇒ None
+			case line ⇒ println("Malformed production: " + line); None
 		} toList
 
 		(state, rules)
 	}
 
-	def execute(state: String, rules: List[Rule], opts: Map[String, String]) : String = {
+	def execute(state: String, rules: List[Rule], opts: Map[String, String]): String = {
 		if (opts contains "debug") println("step: " + state)
-		val matches = rules.flatMap(r => r.lhs.r.findAllIn(state).matchData.map((_,r.rhs))).toArray
+		val matches = rules.flatMap(r ⇒ r.lhs.r.findAllIn(state).matchData.map((_, r.rhs))).toArray
 
 		if (matches isEmpty) return state
 
@@ -63,9 +63,9 @@ object Thue {
 	}
 
 	def batch(state: String, rules: List[Rule], opts: Map[String, String]) = {
-		def loop(strings: Set[String], t: Int) : Set[String] =
-			if (((opts contains "batch") && (strings.size >= opts("batch").toInt)) 
-			 || ((opts contains "times") && (t >= opts("times").toInt))) strings
+		def loop(strings: Set[String], t: Int): Set[String] =
+			if (((opts contains "batch") && (strings.size >= opts("batch").toInt))
+				|| ((opts contains "times") && (t >= opts("times").toInt))) strings
 			else loop(strings + execute(state, rules, opts), t + 1)
 		loop(Set.empty[String], 0)
 	}
